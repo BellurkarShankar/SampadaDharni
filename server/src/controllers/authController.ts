edd3ef9e8f7e1044d5b8db823cf3a958c266b138
shankar bellurkar <sbellurkar@gmail.com>
2025-10-15 18:11:33 +0530

    feat(auth): implement logout

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -143,3 +143,22 @@ export const login = async (req: Request, res: Response): Promise<void> => {
     res.status(500).json(errorResponse);
   }
 };
+
+export const logout = async (req: Request, res: Response): Promise<void> => {
+  try {
+    res.clearCookie("accessToken");
+    res.clearCookie("refreshToken");
+    const logoutSuccessResponse: MyResponse = {
+      success: true,
+      message: "User Logged Out successfully..",
+    };
+    res.status(200).json(logoutSuccessResponse);
+  } catch (error) {
+    const logoutError: MyResponse = {
+      success: false,
+      message: "Internal server error",
+      error: error instanceof Error ? error.message : error,
+    };
+    res.status(500).json(logoutError);
+  }
+};
--- server/src/routes/authRoute.ts
+++ server/src/routes/authRoute.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { login, signUp } from "../controllers/authController";
+import { login, logout, signUp } from "../controllers/authController";
 
 const authRoute = Router();
 
 authRoute.post("/register", signUp);
 authRoute.post("/login", login);
-
+authRoute.post("/logout", logout);
 export default authRoute;
