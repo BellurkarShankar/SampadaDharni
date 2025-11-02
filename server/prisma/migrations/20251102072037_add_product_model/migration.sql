9905fa26c77ac47fbae1f87b0be090845215f986
shankar bellurkar <sbellurkar@gmail.com>
2025-11-02 12:58:14 +0530

    feat(prisma): add Product, Review, and Variant models

---
+++ server/prisma/migrations/20251102072037_add_product_model/migration.sql
@@ -0,0 +1,59 @@
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('MALE', 'FEMALE', 'OTHER');
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "brand" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "gender" "Gender" NOT NULL,
+    "sizes" TEXT[],
+    "colors" TEXT[],
+    "price" DOUBLE PRECISION NOT NULL,
+    "stock" INTEGER NOT NULL,
+    "soldCount" INTEGER NOT NULL,
+    "rating" DOUBLE PRECISION,
+    "tags" TEXT[],
+    "images" TEXT[],
+    "isFeatured" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Review" (
+    "id" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "rating" DOUBLE PRECISION NOT NULL,
+    "comment" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Review_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Variant" (
+    "id" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+    "size" TEXT NOT NULL,
+    "stock" INTEGER NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "images" TEXT[],
+
+    CONSTRAINT "Variant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Variant_productId_color_size_key" ON "Variant"("productId", "color", "size");
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Variant" ADD CONSTRAINT "Variant_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
